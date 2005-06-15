b18a9b30396e5aa6bd52da72d46d59f2659efc49
cmlenz <cmlenz@af82e41b-90c4-0310-8c96-b1721e28e2e2>
2005-06-15 11:10:24 +0000

    Tag the [milestone:0.8.3 0.8.3] release.

--- trac/Settings.py
+++ trac/Settings.py
@@ -1,7 +1,7 @@
 # -*- coding: iso8859-1 -*-
 #
-# Copyright (C) 2004, 2005 Edgewall Software
-# Copyright (C) 2004, 2005 Daniel Lundin <daniel@edgewall.com>
+# Copyright (C) 2004 Edgewall Software
+# Copyright (C) 2004 Daniel Lundin <daniel@edgewall.com>
 #
 # Trac is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License as
@@ -19,63 +19,41 @@
 #
 # Author: Daniel Lundin <daniel@edgewall.com>
 
-from __future__ import generators
+import time
 
-from trac.core import *
-from trac.util import escape
-from trac.web.chrome import INavigationContributor
-from trac.web.main import IRequestHandler
+import perm
+from util import TracError
+from Module import Module
 
-class SettingsModule(Component):
 
-    implements(INavigationContributor, IRequestHandler)
+class Settings(Module):
+    template_name = 'settings.cs'
 
-    _form_fields = ['newsid','name', 'email']
+    _form_fields = ['newsid','name', 'email', 'tz']
 
-    # INavigationContributor methods
+    def render(self):
+        self.req.hdf.setValue('title', 'Settings')
+        action = self.args.get('action')
+        self.log.debug('Session action: %s' % action)
+        if action == 'save':
+            self.save_settings()
+        elif action == 'load':
+            self.load_session()
+        elif action == 'login':
+            self.req.redirect (self.env.href.login())
+        elif action == 'newsession':
+            raise TracError, 'new session'
 
-    def get_active_navigation_item(self, req):
-        return 'settings'
-
-    def get_navigation_items(self, req):
-        yield 'metanav', 'settings', '<a href="%s">Settings</a>' \
-              % escape(self.env.href.settings())
-
-    # IRequestHandler methods
-
-    def match_request(self, req):
-        return req.path_info == '/settings'
-
-    def process_request(self, req):
-        action = req.args.get('action')
-
-        if req.method == 'POST':
-            if action == 'save':
-                self._do_save(req)
-            elif action == 'load':
-                self._do_load(req)
-
-        req.hdf['title'] = 'Settings'
-        req.hdf['settings'] = req.session
-        if req.authname == 'anonymous':
-            req.hdf['settings.session_id'] = req.session.sid
-
-        return 'settings.cs', None
-
-    # Internal methods
-
-    def _do_save(self, req):
+    def save_settings(self):
         for field in self._form_fields:
-            val = req.args.get(field)
+            val = self.args.get(field)
             if val:
-                if field == 'newsid' and val:
-                    req.session.change_sid(val)
+                if field =='newsid':
+                    self.req.session.change_sid(val)
                 else:
-                    req.session[field] = val
-        req.redirect(self.env.href.settings())
+                    self.req.session[field] = val
+        self.req.session.populate_hdf() # Update HDF
 
-    def _do_load(self, req):
-        if req.authname == 'anonymous':
-            oldsid = req.args.get('loadsid')
-            req.session.get_session(oldsid)
-        req.redirect(self.env.href.settings())
+    def load_session(self):
+        oldsid = self.args.get('loadsid')
+        self.req.session.get_session(oldsid)
--- trac/upgrades/db3.py
+++ trac/upgrades/db3.py
@@ -14,5 +14,5 @@ CREATE TABLE attachment (
 
 def do_upgrade(env, ver, cursor):
     cursor.execute(sql)
-    env.config.set('attachment', 'max_size', '262144')
-    env.config.save()
+    env.set_config('attachment', 'max_size', '262144')
+    env.save_config()
