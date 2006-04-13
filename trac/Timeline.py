pkdevbox/trac

commit 2fb2c54960a887e968ff7a06ed2cd8ef464f8449

Don't use the (broken) charset given by `getlocale(locale.LC_TIME)` on windows. Fixes #2971.
git-svn-id: https://svn.edgewall.org/repos/trac/trunk@3138 af82e41b-90c4-0310-8c96-b1721e28e2e2

trac/util/__init__.py
@@ -208,8 +208,8 @@ def format_datetime(t=None, format='%x %X', gmt=False):
             t = time.localtime(int(t))
 
     text = time.strftime(format, t)
-    encoding = locale.getlocale(locale.LC_TIME)[1] or \
-               locale.getpreferredencoding()
+    lc_time_encoding = sys.platform != 'win32' and getlocale(locale.LC_TIME)[1]
+    encoding = lc_time_encoding or locale.getpreferredencoding()
     return unicode(text, encoding, 'replace')
 
 def format_date(t=None, format='%x', gmt=False):

commit 151a4521e9db5be370497fb77c7ebc3f59b57e8e

Unicode consolidation: use `to_unicode(x)` instead of `unicode(x)` when `x` might be a `str` object.
This is especially true when the content may come from a plugin,
e.g. the fix in the `WikiProcessor` will likely fix #3024.

`to_unicode` will always succeed at producing some unicode object,
whereas the basic `unicode(x)` call can raise an `UnicodeDecodeError` 
when given an arbitrarily encoded string.
Thus, `unicode(x)` should only be used when we're confident that `x` 
is already an `unicode` or derived object, like `Markup`, or is some other type that can be safely converted to `unicode`.

This also fixes the problems that happened in TracAdmin 
when `sys.stdin.encoding` was `None` (#3018).

''Last minute, the typo introduced in r3138 in `format_datetime` is fixed,
and mgood actually already fixed #3018 in r3140 :)''

trac/Timeline.py
@@ -177,7 +177,7 @@ class TimelineModule(Component):
                 if isinstance(title, Markup):
                     title = title.plaintext(keeplinebreaks=False)
                 event['title'] = title
-                event['message'] = unicode(message)
+                event['message'] = to_unicode(message)
 
                 if author:
                     # For RSS, author must be an email address
@@ -224,4 +224,4 @@ class TimelineModule(Component):
                                'of events from the <a href="%s">'
                                'Timeline</a></p>',
                                ", ".join(guilty_kinds),
-                               exc.__class__.__name__, unicode(exc), href))
+                               exc.__class__.__name__, to_unicode(exc), href))

trac/db/schema.py
@@ -23,7 +23,7 @@ class Table(object):
         self.columns = []
         self.indices = []
         self.key = key
-        if isinstance(key, (str, unicode)):
+        if isinstance(key, basestring):
             self.key = [key]
 
     def __getitem__(self, objs):

trac/env.py
@@ -111,7 +111,7 @@ class Environment(Component, ComponentManager):
         This is called by the `ComponentManager` base class when a component is
         about to be activated. If this method returns false, the component does
         not get activated."""
-        if not isinstance(cls, (str, unicode)):
+        if not isinstance(cls, basestring):
             component_name = (cls.__module__ + '.' + cls.__name__).lower()
         else:
             component_name = cls.lower()

trac/mimeview/api.py
@@ -22,8 +22,8 @@ from StringIO import StringIO
 
 from trac.core import *
 from trac.config import *
-from trac.util import escape, to_utf8, to_unicode
-from trac.util.markup import Markup, Fragment, html
+from trac.util import to_utf8, to_unicode
+from trac.util.markup import escape, Markup, Fragment, html
 
 
 __all__ = ['get_mimetype', 'is_binary', 'detect_unicode', 'Mimeview',

trac/mimeview/patch.py
@@ -17,7 +17,7 @@
 
 from trac.core import *
 from trac.mimeview.api import content_to_unicode, IHTMLPreviewRenderer
-from trac.util import escape, Markup
+from trac.util.markup import escape, Markup
 from trac.web.chrome import add_stylesheet
 
 __all__ = ['PatchRenderer']

trac/ticket/query.py
@@ -528,9 +528,10 @@ class QueryModule(Component):
         # The most recent query is stored in the user session
         orig_list = rest_list = None
         orig_time = int(time.time())
-        if unicode(query.constraints) != req.session.get('query_constraints'):
+        query_constraints = unicode(query.constraints)
+        if query_constraints != req.session.get('query_constraints'):
             # New query, initialize session vars
-            req.session['query_constraints'] = unicode(query.constraints)
+            req.session['query_constraints'] = query_constraints
             req.session['query_time'] = int(time.time())
             req.session['query_tickets'] = ' '.join([str(t['id']) for t in tickets])
         else:

trac/util/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 # Copyright (C) 2003-2006 Edgewall Software
-# Copyright (C) 2003-2004 Jonas Borgström <jonas@edgewall.com>
+# Copyright (C) 2003-2004 Jonas BorgstrÃ¶m <jonas@edgewall.com>
 # Copyright (C) 2006 Matthew Good <trac@matt-good.net>
 # Copyright (C) 2005-2006 Christian Boos <cboos@neuf.fr>
 # All rights reserved.
@@ -14,7 +14,7 @@
 # individuals. For exact contribution history, see the revision
 # history and logs, available at http://projects.edgewall.com/trac/.
 #
-# Author: Jonas Borgström <jonas@edgewall.com>
+# Author: Jonas BorgstrÃ¶m <jonas@edgewall.com>
 #         Matthew Good <trac@matt-good.net>
 
 import locale
@@ -90,8 +90,8 @@ def to_unicode(text, charset=None, lossy=True):
     will always work, as there's one Unicode character for each byte of
     the input).
     """
-    if isinstance(text, unicode):
-        return text
+    if not isinstance(text, str):
+        return unicode(text)
     errors = lossy and 'replace' or 'strict'
     try:
         if charset:
@@ -208,8 +208,10 @@ def format_datetime(t=None, format='%x %X', gmt=False):
             t = time.localtime(int(t))
 
     text = time.strftime(format, t)
-    lc_time_encoding = sys.platform != 'win32' and getlocale(locale.LC_TIME)[1]
-    encoding = lc_time_encoding or locale.getpreferredencoding()
+    encoding = locale.getpreferredencoding()
+    if sys.platform != 'win32':
+        encoding = locale.getlocale(locale.LC_TIME)[1] or encoding
+        # the above is broken on win32, e.g. we'd get '437' instead of 'cp437'
     return unicode(text, encoding, 'replace')
 
 def format_date(t=None, format='%x', gmt=False):

trac/web/clearsilver.py
@@ -280,7 +280,7 @@ class HDFWrapper:
         object, or a string. In the latter case it is interpreted as name of the
         template file.
         """
-        if isinstance(template, (str, unicode)):
+        if isinstance(template, basestring):
             filename = template
             import neo_cs
             template = neo_cs.CS(self.hdf)

trac/web/main.py
@@ -26,7 +26,8 @@ from trac.config import *
 from trac.core import *
 from trac.env import open_environment
 from trac.perm import PermissionCache, PermissionError
-from trac.util import format_datetime, http_date, Markup
+from trac.util import format_datetime, http_date, to_unicode
+from trac.util.markup import Markup
 from trac.web.api import *
 from trac.web.chrome import Chrome
 from trac.web.clearsilver import HDFWrapper
@@ -181,7 +182,7 @@ class RequestDispatcher(Component):
 
                     req.display(template, content_type or 'text/html')
             except PermissionError, e:
-                raise HTTPForbidden(unicode(e))
+                raise HTTPForbidden(to_unicode(e))
             except TracError, e:
                 raise HTTPInternalError(e.message)
         finally:
@@ -327,9 +328,9 @@ def dispatch_request(environ, start_response):
         traceback.print_exc(file=tb)
 
         if req.hdf:
-            req.hdf['title'] = unicode(e) or 'Error'
+            req.hdf['title'] = to_unicode(e) or 'Error'
             req.hdf['error'] = {
-                'title': unicode(e) or 'Error',
+                'title': to_unicode(e) or 'Error',
                 'type': 'internal',
                 'traceback': tb.getvalue()
             }
@@ -376,7 +377,7 @@ def send_project_index(environ, start_response, parent_dir=None,
                     'href': href(env_name)
                 }
             except Exception, e:
-                proj = {'name': env_name, 'description': unicode(e)}
+                proj = {'name': env_name, 'description': to_unicode(e)}
             projects.append(proj)
         projects.sort(lambda x, y: cmp(x['name'].lower(), y['name'].lower()))
 

trac/wiki/formatter.py
@@ -26,7 +26,7 @@ from StringIO import StringIO
 from trac.core import *
 from trac.mimeview import *
 from trac.wiki.api import WikiSystem, IWikiChangeListener, IWikiMacroProvider
-from trac.util import shorten_line
+from trac.util import shorten_line, to_unicode
 from trac.util.markup import escape, Markup, Element, html
 
 __all__ = ['wiki_to_html', 'wiki_to_oneliner', 'wiki_to_outline', 'Formatter' ]
@@ -128,7 +128,7 @@ class WikiProcessor(object):
                 text = html.SPAN(class_='code-block')[content_for_span]
             elif interrupt_paragraph:
                 text = "</p>%s<p>" % text
-        return unicode(text)
+        return to_unicode(text)
 
 
 class Formatter(object):
@@ -344,8 +344,8 @@ class Formatter(object):
         # check first for an alias defined in trac.ini
         ns = self.env.config.get('intertrac', ns.upper()) or ns
         if ns in self.wiki.link_resolvers:
-            return unicode(self.wiki.link_resolvers[ns](self, ns, target,
-                                                        escape(label, False)))
+            return to_unicode(self.wiki.link_resolvers[ns](
+                self, ns, target, escape(label, False)))
         elif target.startswith('//') or ns == "mailto":
             return self._make_ext_link(ns+':'+target, label)
         else:
@@ -599,10 +599,11 @@ class Formatter(object):
                 if match[0] == '!':
                     return match[1:]
                 if itype in self.wiki.external_handlers:
-                    return unicode(self.wiki.external_handlers[itype](self, match,
-                                                                      fullmatch))
+                    return to_unicode(self.wiki.external_handlers[itype](
+                        self, match, fullmatch))
                 else:
-                    return getattr(self, '_' + itype + '_formatter')(match, fullmatch)
+                    return getattr(self, '_' + itype + '_formatter')(match,
+                                                                     fullmatch)
     def handle_code_block(self, line):
         if line.strip() == Formatter.STARTBLOCK:
             self.in_code_block += 1

trac/wiki/tests/formatter.py
@@ -7,6 +7,7 @@ from trac.core import *
 from trac.test import Mock
 from trac.wiki.formatter import Formatter, OneLinerFormatter
 from trac.wiki.macros import WikiMacroBase
+from trac.util import to_unicode
 from trac.util.markup import html
 
 # We need to supply our own macro because the real macros
@@ -104,7 +105,7 @@ class WikiTestCase(unittest.TestCase):
         except AssertionError, e:
             raise AssertionError('%s\n\n%s:%s: for the input '
                                  '(formatter flavor was "%s")' \
-                                 % (unicode(e), self.file, self.line,
+                                 % (to_unicode(e), self.file, self.line,
                                     formatter.flavor))
         
     def formatter(self):