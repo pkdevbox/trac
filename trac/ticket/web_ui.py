pkdevbox/trac

commit 5c7a4937a6402230ae9b673e9387eaa32f3383dd

Fix some breakage introduced by r4347: the ''version'' field and the ticket ''version'' conflicted, resulting in an incorrect preview. All the ticket fields arguments are now prefixed by ''field_''.
git-svn-id: https://svn.edgewall.org/repos/trac/trunk@4387 af82e41b-90c4-0310-8c96-b1721e28e2e2

trac/ticket/web_ui.py
@@ -120,15 +120,15 @@ class TicketModule(Component):
         data = {}
         db = self.env.get_db_cnx()
 
-        if req.method == 'POST' and 'owner' in req.args and \
+        if req.method == 'POST' and 'field_owner' in req.args and \
                'TICKET_MODIFY' not in req.perm:
-            del req.args['owner']
+            del req.args['field_owner']
 
         if req.method == 'POST' and 'preview' not in req.args:
             self._do_create(req, db)
 
         ticket = Ticket(self.env, db=db)
-        ticket.populate(req.args)
+        self._populate(req, ticket)
         ticket.values['reporter'] = get_reporter_id(req, 'reporter')
         data['ticket'] = ticket
 
@@ -198,7 +198,7 @@ class TicketModule(Component):
                 self._do_save(req, db, ticket)
             else:
                 # Use user supplied values
-                ticket.populate(req.args)
+                self._populate(req, ticket)
                 self._validate_ticket(req, ticket)
 
                 data['action'] = action
@@ -250,6 +250,10 @@ class TicketModule(Component):
                      conversion[3])
 
         return 'ticket_view.html', data, None
+
+    def _populate(self, req, ticket):
+        ticket.populate(dict([(k[6:],v) for k,v in req.args.iteritems()
+                              if k.startswith('field_')]))
 
     def _get_history(self, ticket, db):
         history = []
@@ -623,11 +627,11 @@ class TicketModule(Component):
                     raise InvalidTicket("Invalid ticket: %s" % message)
 
     def _do_create(self, req, db):
-        if 'summary' not in req.args:
+        if 'field_summary' not in req.args:
             raise TracError('Tickets must contain a summary.')
 
         ticket = Ticket(self.env, db=db)
-        ticket.populate(req.args)
+        self._populate(req, ticket)
         ticket.values['reporter'] = get_reporter_id(req, 'reporter')
         self._validate_ticket(req, ticket)
 
@@ -651,13 +655,13 @@ class TicketModule(Component):
     def _do_save(self, req, db, ticket):
         if 'TICKET_CHGPROP' in req.perm:
             # TICKET_CHGPROP gives permission to edit the ticket
-            if not req.args.get('summary'):
+            if not req.args.get('field_summary'):
                 raise TracError('Tickets must contain summary.')
 
-            if req.args.has_key('description') or req.args.has_key('reporter'):
+            if 'field_description' in req.args or 'field_reporter' in req.args:
                 req.perm.require('TICKET_ADMIN')
 
-            ticket.populate(req.args)
+            self._populate(req, ticket)
         else:
             req.perm.require('TICKET_APPEND')
 
@@ -795,7 +799,7 @@ class TicketModule(Component):
 
         if version is not None:
             ticket.values.update(values)
-
+            
         data.update({
             'fields': fields, 'changes': changes, 'field_types': types,
             'replies': replies, 'cnum': cnum + 1,

htdocs/css/ticket.css
@@ -2,7 +2,7 @@
 
 #content.ticket { width: 700px; max-width: 100% }
 
-#newticket #description { width: 100% }
+#newticket #field-description { width: 100% }
 #newticket #properties { width: 100% }
 
 #ticket {

templates/ticket_new.html
@@ -21,17 +21,17 @@
 
       <form id="newticket" method="post" action="#preview">
         <div py:if="req.authname == 'anonymous'" class="field">
-          <label for="reporter">Your email or username:</label><br />
-          <input type="text" id="reporter" name="reporter" size="40" value="${ticket.reporter}" /><br />
+          <label for="field-reporter">Your email or username:</label><br />
+          <input type="text" id="field-reporter" name="field_reporter" size="40" value="${ticket.reporter}" /><br />
         </div>
         <div class="field">
-          <label for="summary">Short summary:</label><br />
-          <input id="summary" type="text" name="summary" size="80" value="${ticket.summary}"/>
+          <label for="field-summary">Short summary:</label><br />
+          <input id="field-summary" type="text" name="field_summary" size="80" value="${ticket.summary}"/>
         </div>
         <div class="field">
-          <label for="description">Full description (you may use <a tabindex="42"
+          <label for="field-description">Full description (you may use <a tabindex="42"
                  href="${href.wiki('WikiFormatting')}">WikiFormatting</a> here):</label><br />
-          <textarea id="description" name="description" class="wikitext" rows="10"
+          <textarea id="field-description" name="field_description" class="wikitext" rows="10"
                  cols="78">${ticket.description}</textarea>
           <fieldset py:if="ticket.description" id="preview">
             <legend>Description Preview</legend>
@@ -49,31 +49,31 @@
                 py:with="fullrow = len(row) == 1">
               <py:for each="idx, field in enumerate(row)">
                 <th class="col${idx + 1}" py:if="idx == 0 or not fullrow">
-                  <label for="${field.name}" py:if="field"
+                  <label for="field-${field.name}" py:if="field"
                          py:strip="field.type == 'radio'">${field.label or field.name}:</label>
                 </th>
                 <td class="col${idx + 1}" py:if="idx == 0 or not fullrow"
                     py:attrs="{'colspan': fullrow and 3 or None}">
                   <py:choose test="field.type" py:if="field">
-                    <select py:when="'select'" id="${field.name}" name="${field.name}">
+                    <select py:when="'select'" id="field-${field.name}" name="field_${field.name}">
                       <option py:if="field.optional"></option>
                       <option py:for="option in field.options"
                               selected="${ticket[field.name] == option or None}"
                               py:content="option"></option>
                     </select>
-                    <textarea py:when="'textarea'" id="${field.name}" name="${field.name}"
+                    <textarea py:when="'textarea'" id="field-${field.name}" name="field_${field.name}"
                               cols="${field.width}" rows="${field.height}"
                               py:content="ticket[field.name]"></textarea>
-                    <input py:when="'checkbox'" type="checkbox" id="${field.name}"
-                           name="${field.name}" value="${field.value}" />
+                    <input py:when="'checkbox'" type="checkbox" id="field-${field.name}"
+                           name="field_${field.name}" value="${field.value}" />
                     <label py:when="'radio'"
                            py:for="idx, option in enumerate(field.options)">
-                      <input type="radio" name="${field.name}" value="${option}"
+                      <input type="radio" name="field_${field.name}" value="${option}"
                              checked="${ticket[field.name] == option or None}" />
                       ${option}
                     </label>
-                    <input py:otherwise="" type="text" id="${field.name}"
-                           name="${field.name}" value="${ticket[field.name]}" />
+                    <input py:otherwise="" type="text" id="field-${field.name}"
+                           name="field_${field.name}" value="${ticket[field.name]}" />
                   </py:choose>
                 </td>
               </py:for>

templates/ticket_view.html
@@ -194,24 +194,24 @@ ${comment}</textarea></p>
           <legend>Change Properties</legend>
           <table>
             <tr>
-              <th><label for="summary">Summary:</label></th>
+              <th><label for="field-summary">Summary:</label></th>
               <td class="fullrow" colspan="3">
-                <input type="text" id="summary" name="summary" value="${ticket.summary}"
+                <input type="text" id="field-summary" name="field_summary" value="${ticket.summary}"
                   size="70" />
               </td>
             </tr>
             <py:if test="'TICKET_ADMIN' in perm">
               <tr>
-                <th><label for="reporter">Reporter:</label></th>
+                <th><label for="field-reporter">Reporter:</label></th>
                 <td class="fullrow" colspan="3">
-                  <input type="text" id="reporter" name="reporter"
+                  <input type="text" id="field-reporter" name="field_reporter"
                     value="${ticket.reporter}" size="70" />
                 </td>
               </tr>
               <tr>
-                <th><label for="description">Description:</label></th>
+                <th><label for="field-description">Description:</label></th>
                 <td class="fullrow" colspan="3">
-                  <textarea id="description" name="description" class="wikitext"
+                  <textarea id="field-description" name="field_description" class="wikitext"
                     rows="10" cols="68" py:content="ticket.description"></textarea>
                 </td>
               </tr>
@@ -220,34 +220,34 @@ ${comment}</textarea></p>
                 py:with="fullrow = len(row) == 1">
               <py:for each="idx, field in enumerate(row)">
                 <th class="col${idx + 1}" py:if="idx == 0 or not fullrow">
-                  <label for="${field.name}" py:if="field"
+                  <label for="field-${field.name}" py:if="field"
                     py:strip="field.type == 'radio'">${field.label or field.name}:</label>
                 </th>
                 <td class="col${idx + 1}" py:if="idx == 0 or not fullrow"
                     py:attrs="{'colspan': fullrow and 3 or None}">
                   <py:choose test="field.type" py:if="field">
-                    <select py:when="'select'" id="${field.name}" name="${field.name}">
+                    <select py:when="'select'" id="field-${field.name}" name="field_${field.name}">
                       <option py:if="field.optional"></option>
                       <option py:for="option in field.options"
                         selected="${ticket[field.name] == option or None}"
                         py:content="option"></option>
                     </select>
-                    <textarea py:when="'textarea'" id="${field.name}" name="${field.name}"
+                    <textarea py:when="'textarea'" id="field-${field.name}" name="field_${field.name}"
                       cols="${field.width}" rows="${field.height}"
                       py:content="ticket[field.name]"></textarea>
                     <span py:when="'checkbox'">
-                      <input type="checkbox" id="${field.name}" name="${field.name}"
+                      <input type="checkbox" id="field-${field.name}" name="field_${field.name}"
                              checked="${ticket[field.name] == '1' and 'checked' or None}" value="1" />
-                      <input type="hidden" name="checkbox_${field.name}" value="1" />
+                      <input type="hidden" name="field_checkbox_${field.name}" value="1" />
                     </span>
                     <label py:when="'radio'"
                       py:for="idx, option in enumerate(field.options)">
-                      <input type="radio" name="${field.name}" value="${option}"
+                      <input type="radio" name="field_${field.name}" value="${option}"
                         checked="${ticket[field.name] == option or None}" />
                       ${option}
                     </label>
-                    <input py:otherwise="" type="text" id="${field.name}"
-                      name="${field.name}" value="${ticket[field.name]}" />
+                    <input py:otherwise="" type="text" id="field-${field.name}"
+                      field_name="${field.name}" value="${ticket[field.name]}" />
                   </py:choose>
                 </td>
               </py:for>

commit 3cc2b815c2ed0aa90342be517470dc960324f13a

fix reading of reporter ticket field broken by r4387 (fixes #4936, thanks Eli)

trac/ticket/web_ui.py
@@ -277,7 +277,7 @@ class TicketModule(Component):
         context = context('ticket', ticket.id, resource=ticket)
         
         self._populate(req, ticket)
-        ticket.values['reporter'] = get_reporter_id(req, 'reporter')
+        ticket.values['reporter'] = get_reporter_id(req, 'field_reporter')
 
         data = {}
         data['ticket'] = ticket
@@ -672,7 +672,7 @@ class TicketModule(Component):
             raise TracError('Tickets must contain a summary.')
 
         self._populate(req, ticket)
-        ticket.values['reporter'] = get_reporter_id(req, 'reporter')
+        ticket.values['reporter'] = get_reporter_id(req, 'field_reporter')
         self._validate_ticket(req, ticket)
 
         ticket.insert(db=context.db)